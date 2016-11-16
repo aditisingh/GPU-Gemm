bb67de1074d03f6bec5e92ae11e408601b9d8a88
Aditi Singh <787aditi@gmail.com>
2016-11-16 14:38:36 -0600

    Commented

--- gemm.cu
+++ gemm.cu
@@ -46,15 +46,15 @@
 		__shared__ float S2[TILE_HEIGHT][TILE_WIDTH];	//to store tiles for array 2
 
 		//threads x and y index for the current block
-		unsigned int tx=threadIdx.x;
+		unsigned int tx=threadIdx.x;	
 		unsigned int ty=threadIdx.y;
 
 		unsigned int c=blockIdx.x*blockDim.x + threadIdx.x;	//row value using x-index of current thread
 		unsigned int r=blockIdx.y*blockDim.y + threadIdx.y;	//column value using y-index of current thread
 
-		unsigned int idx=c*rows1+r;	//column major index, using row and column value
+		unsigned int idx=c*rows1+r;				//column major index, using row and column value
 		
-		float val=0;//register to multiplication result
+		float val=0;		//register to store multiplication result initialized to zero
 
 		for(int m=0; m<1+((rows2-1)/TILE_WIDTH);m++)	//going over all tiles one by one, with each m
 		{
@@ -62,42 +62,38 @@
 			int var1=m*TILE_WIDTH+tx ;		//x thread value for current tile
 			int var2=m*TILE_WIDTH+ty ;		//y thread value for current tile
 			
-			if (r < rows1 && var1 < rows2)	//if the value is associated to a valid matrix coordinate then store it to shared, else store zero
-				S1[ty][tx]=array1[r + var1*rows1];	//storing a "valid" value from array to shared memory
+			//copying a tile from array1
+			if (r < rows1 && var1 < rows2)		//if the value is associated to a valid matrix coordinate in array1 then store it to shared memory S1
+				S1[ty][tx]=array1[r + var1*rows1];//storing a "valid" value from array to shared memory
 			else
 					S1[ty][tx]=0;					//storing zero, since there is no valid value
-       		__syncthreads();						//syncing all threads once shared memory S1 is stored
+       			__syncthreads();						//syncing all threads once shared memory S1 is stored
 			
-       		if(c<cols2 && var2< rows2)
-      			S2[ty][tx]=array2[var2+rows2*c];
-      		else 
-      			{
-      			S2[ty][tx]=0;
-      			// printf("S2 is zero\n");
-      		}
-			__syncthreads();
+			//copying a tile from array2
+	       		if(c < cols2 && var2 < rows2)	//if value is associates to a valid matrix coordinate in array2 then store it to shared memory S2
+	      			S2[ty][tx]=array2[var2+rows2*c];	//storing the valid value
+	      		else 
+	      			S2[ty][tx]=0;		//storing zero, since no valid value
+			__syncthreads();		//synchronizing threads
 			
 
-			for(int i=0; i<TILE_WIDTH;i++)
-				val+=S1[ty][i]*S2[i][tx];
-			__syncthreads();
+			for(int i=0; i<TILE_WIDTH;i++)	//going over entire tile, ty row in S1 and tx column in S2
+				val+=S1[ty][i]*S2[i][tx];	//and multiplying elements
+			__syncthreads();		//synchronizing threads
 
 		}
 		
-		if(r < rows1 && c< cols2)	
-			array3[idx]=val;
+		if(r < rows1 && c< cols2)	//removing degenerate cases
+			array3[idx]=val;	//saving multiplication result to global memory
 			
 	}
 
 	int main(int argc, char* argv[])
 	{
-		if(argc != 4) //there should be four arguments
+		if(argc != 4) //there should be four arguments, Usage: prog matrix1.mtx matrix2.mtx matrix3.mtx
 		return 1; //exit and return an error
 
-		time_t reading_start=time(NULL);
-
 		ifstream infile_A, infile_B;	//reading the input matrices
-		
 		
 		//READING matrix A
 		infile_A.open(argv[1],ios::binary|ios::in|ios::ate);
@@ -107,14 +103,14 @@
 		infile_A.seekg(0,ios::beg);
 		
 		//memory allocation
-		matrix M_A;	
-		infile_A.read(reinterpret_cast<char*>(&M_A),2*sizeof(unsigned int));
+		matrix M_A;	//to store rows and cols
+		infile_A.read(reinterpret_cast<char*>(&M_A),2*sizeof(unsigned int));//read cols and rows value
 		
 
-		float* array_A=(float*)malloc(M_A.rows*M_A.cols*sizeof(float));	//column major
-		infile_A.read(reinterpret_cast<char*>(array_A),M_A.rows*M_A.cols*sizeof(float));
+		float* array_A=(float*)malloc(M_A.rows*M_A.cols*sizeof(float));	//array_A stores the first matrix
+		infile_A.read(reinterpret_cast<char*>(array_A),M_A.rows*M_A.cols*sizeof(float));//reading the matrix elements in column major format
 		
-		infile_A.close();
+		infile_A.close();//closing the first file
 
 		//READING matrix B
 		infile_B.open(argv[2],ios::binary|ios::in|ios::ate);
@@ -124,141 +120,136 @@
 		infile_B.seekg(0,ios::beg);
 		
 		//memory allocation
-		matrix M_B;
-		infile_B.read(reinterpret_cast<char*>(&M_B),2*sizeof(unsigned int));
+		matrix M_B;	//to store rows and cols for second matrix
+		infile_B.read(reinterpret_cast<char*>(&M_B),2*sizeof(unsigned int));//reading cols and rows valus
 
-		float* array_B=(float*)malloc(M_B.rows*M_B.cols*sizeof(float));	//column major
+		float* array_B=(float*)malloc(M_B.rows*M_B.cols*sizeof(float));	//array_B stores the second matrix
 
-		infile_B.read(reinterpret_cast<char*>(array_B),M_B.rows*M_B.cols*sizeof(float));
+		infile_B.read(reinterpret_cast<char*>(array_B),M_B.rows*M_B.cols*sizeof(float));//reading the matrix elements in column major format
 		
-		infile_B.close();
+		infile_B.close();//closing second file after reading
 
-		if(M_A.cols!=M_B.rows)
+		if(M_A.cols!=M_B.rows) //checking if the two matrices can be multiplied
 		{
 			cout<<"Illegal matrix sizes: "<<M_A.cols<<" != "<<M_B.rows<<endl;
 			return 1;
 		}
 
-		float* array_C=(float*)malloc(M_A.rows*M_B.cols*sizeof(float));//gpu result
+		float* array_C=(float*)malloc(M_A.rows*M_B.cols*sizeof(float));//array to store gpu result in column major format
 		
-		float* array_D=(float*)malloc(M_A.rows*M_B.cols*sizeof(float));//cublas result
+		float* array_D=(float*)malloc(M_A.rows*M_B.cols*sizeof(float));//arary to store cublas result in column major format
 		
-		time_t reading_end = time(NULL);
 
 
-		//GPU DEVICE PROPERTIES
+		//GPU DEVICE PROPERTIES and selecting a GPU for calculation
 		int nDevices;
 		HANDLE_ERROR(cudaGetDeviceCount(&nDevices));
 
 		cudaDeviceProp prop;
 	   	HANDLE_ERROR(cudaGetDeviceProperties(&prop, 0));	//using GPU0
 
-	   	//BLOCK AND GRID SIZE
-	   	float thread_block=sqrt(prop.maxThreadsPerBlock);
+	   	//BLOCK AND GRID SIZE DECLARATION
+	   	float thread_block=sqrt(prop.maxThreadsPerBlock);	//2D blocks used
 		dim3 DimGrid(ceil(M_B.cols/thread_block),ceil(M_A.rows/thread_block),1); //image saved as a 2D grid
 		dim3 DimBlock(thread_block,thread_block,1);
 
-		size_t Sbytes = 2* DimBlock.x * DimBlock.y ;
+		size_t Sbytes = 2* DimBlock.x * DimBlock.y ;	//2 arrays used in the calculation, hence 2 * DimBlock.x * DimBlock.y
 		
+		//Checking if sufficient shared memory available or not
 
-		cudaDeviceProp props;
-		cudaGetDeviceProperties(&props, 0);
-
-		if(props.sharedMemPerBlock < Sbytes){
+		if(prop.sharedMemPerBlock < Sbytes){
 			std::cout<<"ERROR: insufficient shared memory"<<std::endl;
 			exit(1);
 		}
 
 		//GPU MEMORY ALLOCATION
-		float *array_A_gpu, *array_B_gpu, *array_C_gpu, *array_D_gpu;
+		float *array_A_gpu, *array_B_gpu, *array_C_gpu, *array_D_gpu;	//gpu arrays declared
 	   
-		HANDLE_ERROR(cudaMalloc(&array_A_gpu,M_A.rows*M_A.cols*sizeof(float))); //allocate space to store convolution result
+		HANDLE_ERROR(cudaMalloc(&array_A_gpu,M_A.rows*M_A.cols*sizeof(float))); //allocate space to store arrayA
 
-		HANDLE_ERROR(cudaMalloc(&array_B_gpu,M_B.rows*M_B.cols*sizeof(float))); //allocate space to store convolution temporary
+		HANDLE_ERROR(cudaMalloc(&array_B_gpu,M_B.rows*M_B.cols*sizeof(float))); //allocate space to store arrayB
 
-		HANDLE_ERROR(cudaMalloc(&array_C_gpu,M_A.rows*M_B.cols*sizeof(float))); //allocate space to copy image to GPU memory
+		HANDLE_ERROR(cudaMalloc(&array_C_gpu,M_A.rows*M_B.cols*sizeof(float))); //allocate space to store gpu result
 
-		HANDLE_ERROR(cudaMalloc(&array_D_gpu,M_A.rows*M_B.cols*sizeof(float))); //allocate space to copy image to GPU memory
+		HANDLE_ERROR(cudaMalloc(&array_D_gpu,M_A.rows*M_B.cols*sizeof(float))); //allocate space to store cublas result
 
 
 		//COPY TO GPU MEMORY
-		HANDLE_ERROR(cudaMemcpy(array_A_gpu, array_A, M_A.rows*M_A.cols*sizeof(float), cudaMemcpyHostToDevice));//copy input image from global to gpu
+		HANDLE_ERROR(cudaMemcpy(array_A_gpu, array_A, M_A.rows*M_A.cols*sizeof(float), cudaMemcpyHostToDevice));//copy arrayA to gpu
 
-		HANDLE_ERROR(cudaMemcpy(array_B_gpu, array_B, M_B.rows*M_B.cols*sizeof(float), cudaMemcpyHostToDevice));//copy the kernel0 host to device
+		HANDLE_ERROR(cudaMemcpy(array_B_gpu, array_B, M_B.rows*M_B.cols*sizeof(float), cudaMemcpyHostToDevice));//copy arrayB to gpu
 
-		HANDLE_ERROR(cudaMemcpy(array_C_gpu, array_C, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyHostToDevice));//copy kernel1 host to device
+		HANDLE_ERROR(cudaMemcpy(array_C_gpu, array_C, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyHostToDevice));//copy arrayC to gpu
 
-		HANDLE_ERROR(cudaMemcpy(array_D_gpu, array_D, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyHostToDevice));//copy kernel1 host to device
+		HANDLE_ERROR(cudaMemcpy(array_D_gpu, array_D, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyHostToDevice));//copy arrayD to gpu
 
-		time_t memory_transfers=time(NULL);
 
-		//time measurement
+		//time measurement for matrix multiplication
 		cudaEvent_t start1, stop1;
 	 	
 	 	cudaEventCreate(&start1);
 		cudaEventCreate(&stop1);
 		
-		//MATRIX MULTIPLICATION
+		//MATRIX MULTIPLICATION USING KERNEL
 		cudaEventRecord(start1);
-		matrix_mult<<<DimGrid, DimBlock, Sbytes>>>(array_A_gpu,M_A.rows,M_A.cols,array_B_gpu,M_B.rows,M_B.cols,array_C_gpu);
+		matrix_mult<<<DimGrid, DimBlock, Sbytes>>>(array_A_gpu,M_A.rows,M_A.cols,array_B_gpu,M_B.rows,M_B.cols,array_C_gpu);//calling the kernel
 		cudaEventRecord(stop1);
 
-		time_t mult_end = time(NULL);
-
 		cudaEventSynchronize(stop1);
-		float milliseconds1 = 0, milliseconds2 = 0;
+
+		float milliseconds1 = 0, milliseconds2 = 0;//storing the execution time in milliseconds
 		
-		cudaEventElapsedTime(&milliseconds1, start1, stop1);
-		cout<<"time taken by GPU = "<<milliseconds1<<" ms"<<endl;
+		cudaEventElapsedTime(&milliseconds1, start1, stop1);//get the time in milliseconds
+		cout<<"time taken by GPU = "<<milliseconds1<<" ms"<<endl;//printing time taken by GPU
 
 		//copy to CPU MEMORY
-		HANDLE_ERROR(cudaMemcpy(array_C, array_C_gpu, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyDeviceToHost));//copy kernel1 host to device
+		HANDLE_ERROR(cudaMemcpy(array_C, array_C_gpu, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyDeviceToHost));//copying result of multiplication from gpu to cpu
 
 		//Creating handle for CUBLAS
 		cublasHandle_t handle;
 		cublasCreate(&handle);	
 
+		//parameter declaration for cublas implementation
 		float alpha = 1.0;
 		float beta = 0.0;
 	    
-	    cudaEvent_t start2, stop2;
+		//cublas time measurement
+	    	cudaEvent_t start2, stop2;
 	 	
 	 	cudaEventCreate(&start2);
 		cudaEventCreate(&stop2);
 
+		//MATRIX MULTIPLICATION USING CUBLAS 
 		cudaEventRecord(start2);
 		cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, M_A.rows, M_B.cols, M_A.cols, &alpha, array_A_gpu, M_A.rows, array_B_gpu, M_B.rows, &beta, array_D_gpu, M_A.rows);
 		cudaEventRecord(stop2);
 
 		cudaEventSynchronize(stop2);
 
-		cudaEventElapsedTime(&milliseconds2, start2, stop2);
-		cout<<"time taken by CUBLAS= "<<milliseconds2<<" ms"<<endl;
+		cudaEventElapsedTime(&milliseconds2, start2, stop2);//get the time in milliseconds
+		cout<<"time taken by CUBLAS= "<<milliseconds2<<" ms"<<endl;//printing time taken by CUBLAS
 		
 		//copy to CPU MEMORY
 	    
-	    HANDLE_ERROR(cudaMemcpy(array_D, array_D_gpu, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyDeviceToHost));//copy kernel1 host to device
+	    	HANDLE_ERROR(cudaMemcpy(array_D, array_D_gpu, M_A.rows*M_B.cols*sizeof(float), cudaMemcpyDeviceToHost));//copy result of multiplication using CUBLAS from gpu to cpu
 
+		//CALCULATING MEAN SQUARED ERROR IN BOTH METHODS OF MATRIX MULTIPLICATION
 		float mse=0; //mean squared error;
 
 		for(int i=0; i<M_A.rows*M_B.cols;i++)
-			mse=mse+(array_C[i]-array_D[i])*(array_C[i]-array_D[i]);
+			mse=mse+(array_C[i]-array_D[i])*(array_C[i]-array_D[i]);//calculating element by element
+		
+		mse=mse/(M_A.rows*M_B.cols);	//taking the mean of squared error
 			
-		cout<<endl<<"Mean square error = "<<mse<<endl;
+		cout<<endl<<"Mean square error = "<<mse<<endl;//printing out the mean squared error
 
 		//SAVING THE OUTPUT MATRIX
 		ofstream ofile(argv[3], ios::binary);
 
-		ofile.write((char*) &M_A.rows, sizeof(unsigned int));
-		ofile.write((char*) &M_B.cols, sizeof(unsigned int));	
-		ofile.write((char*) array_C , M_A.rows*M_B.cols*sizeof(float))	;
+		ofile.write((char*) &M_A.rows, sizeof(unsigned int));//writing the rows
+		ofile.write((char*) &M_B.cols, sizeof(unsigned int));//writing the cols
+		ofile.write((char*) array_C , M_A.rows*M_B.cols*sizeof(float));//writing all elements
 
-		time_t saved = time(NULL);
-
-		// cout<<"Matrix reading     :"<<double(reading_end - reading_start)<<" secs"<<endl;
-		// cout<<"Memory Transfers   :"<<double(memory_transfers - reading_end)<<" secs"<<endl;
-		// cout<<"Multiplication done:"<<double(mult_end - memory_transfers)<<" secs"<<endl;
-		// cout<<"Matrix saving      :"<<double(saved - mult_end)<<" secs"<<endl;
 
 		return 0;
 	}
